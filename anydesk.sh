5e0d9fe05a8ecb85cc77eb25622577994d9056f9
vedavyas <82499701+Vedavyaskota@users.noreply.github.com>
2021-10-03 00:11:12 +0530

    Add files via upload

---
+++ anydesk.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt -y upgrade -y
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update -y
+sudo apt install anydesk -y
